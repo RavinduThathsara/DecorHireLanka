ec575201d791189387aa4b5bde3ec850b0b9b10f
Munasinghe MART <ravinduthathsara38@gmail.com>
2026-06-08 22:46:43 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://decorhirelanka-backend.onrender.com",
 });
 
 // Helpers for token
